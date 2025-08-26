d7cdeca97fccec0124abdef2ebe88077d9476b2e
Indriani <indrianirais948@gmail.com>
2025-08-26 11:46:51 +0900

    Biodata

---
+++ KULIAH/DART/flutter_application_1/lib/Latihan1.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "indriani";
+  int umur = 20;
+  double berat_badan = 57.5;
+  num Tinggi_badan = 160;
+  dynamic prodi = "informatika";
+
+  print("name : $name");
+  print(umur.toString().runtimeType);
+  print(berat_badan);
+  print(Tinggi_badan);
+  print("prodi : $prodi");
+}
